cad3e7419aabc59795fb044370a0cbac6f2d4403
MrKay12 <quentin.lepoutre12@gmail.com>
2024-10-24 09:48:21 +0200

    task 1 + most of task 2

---
+++ part2/app/models/place.py
@@ -0,0 +1,34 @@
+from basemodel import BaseModel
+from app.models.user import User
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+        
+    def validate(self):
+        if not (self.title and len(self.title) <= 100):
+            raise ValueError("Title is required and must be less than or equal to 100 characters.")
+        if self.price <= 0:
+            raise ValueError("Price must be a positive value.")
+        if not (-90.0 <= self.latitude <= 90.0):
+            raise ValueError("Latitude must be between -90.0 and 90.0.")
+        if not (-180.0 <= self.longitude <= 180.0):
+            raise ValueError("Longitude must be between -180.0 and 180.0.")
+        if not isinstance(self.owner, User):
+            raise ValueError("Owner must be a valid User instance.")
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
---
+++ part2/app/models/review.py
@@ -0,0 +1,23 @@
+from basemodel import BaseModel
+from app.models.place import Place
+from app.models.user import User
+
+
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
+        self.validate()
+
+    def validate(self):
+        if not self.text:
+            raise ValueError("Review text is required.")
+        if not (1 <= self.rating <= 5):
+            raise ValueError("Rating must be between 1 and 5.")
+        if not isinstance(self.place, Place):
+            raise ValueError("Place must be a valid Place instance.")
+        if not isinstance(self.user, User):
+            raise ValueError("User must be a valid User instance.")
